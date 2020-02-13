1aa5ea9f874e8f4d12949d11656dd3a2db7622a1
Rommiel John <rfrancisco6@dxc.com>
2020-02-13 22:02:35 +0800

    Updates

---
+++ precache-manifest.fa336900031794a7ec7946dbcb45cb94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3ffffeca42cded4390cb3b55b40b8e",
+    "url": "/rommiel/Travelgram.git/index.html"
+  },
+  {
+    "revision": "9f393fd87addff046639",
+    "url": "/rommiel/Travelgram.git/static/css/main.4b5c3934.chunk.css"
+  },
+  {
+    "revision": "051d57c13f18751d0dc8",
+    "url": "/rommiel/Travelgram.git/static/js/2.ff830c0b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rommiel/Travelgram.git/static/js/2.ff830c0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f393fd87addff046639",
+    "url": "/rommiel/Travelgram.git/static/js/main.23595111.chunk.js"
+  },
+  {
+    "revision": "b5794588b11aa2cfdbff",
+    "url": "/rommiel/Travelgram.git/static/js/runtime-main.61bd33fa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf7bfe48c38ab72cceed8e6d0b538b83.js"
+  "/rommiel/Travelgram.git/precache-manifest.fa336900031794a7ec7946dbcb45cb94.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rommiel/Travelgram.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
