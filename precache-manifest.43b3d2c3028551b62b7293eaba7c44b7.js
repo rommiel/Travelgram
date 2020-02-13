3f689e86147f93c296ddf1b9dbc988173aba7170
Rommiel John <rfrancisco6@dxc.com>
2020-02-13 22:04:34 +0800

    Updates

---
+++ precache-manifest.43b3d2c3028551b62b7293eaba7c44b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c4025882ad4b7496a5c94a5c2478a9",
+    "url": "/rommiel/Travelgram/index.html"
+  },
+  {
+    "revision": "9f393fd87addff046639",
+    "url": "/rommiel/Travelgram/static/css/main.4b5c3934.chunk.css"
+  },
+  {
+    "revision": "051d57c13f18751d0dc8",
+    "url": "/rommiel/Travelgram/static/js/2.ff830c0b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rommiel/Travelgram/static/js/2.ff830c0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f393fd87addff046639",
+    "url": "/rommiel/Travelgram/static/js/main.23595111.chunk.js"
+  },
+  {
+    "revision": "bef7d935d8d63ded5e72",
+    "url": "/rommiel/Travelgram/static/js/runtime-main.6573a5b8.js"
+  }
+]);
